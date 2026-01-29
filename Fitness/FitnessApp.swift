92c7ae47fc920c1f07558bb1434cc7516a178e09
GabrielaAlcaraz <gabriela.alcaraz@cetys.mx>
2026-01-29 14:23:16 -0800

    Initial Commit

---
+++ Fitness/FitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessApp.swift
+//  Fitness
+//
+//  Created by Gabriela Sanchez on 29/01/26.
+//
+
+import SwiftUI
+
+@main
+struct FitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
